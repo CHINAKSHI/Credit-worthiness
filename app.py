7c7918aa2a31d00c53adb53ba4c26139842e6363
CHINAKSHI <c.chinakshi@gmail.com>
2025-06-28 22:42:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,6 @@ def predict():
     # Show result on the same page
     return render_template('index.html', prediction_text=result)
 
-# Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
